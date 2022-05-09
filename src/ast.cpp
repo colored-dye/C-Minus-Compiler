aaf4cacf2cb60aac95be7aa20d9541e9b78143d3
sio-2 <2586457570@qq.com>
2022-05-09 12:52:03 +0800

    init ast class

---
+++ src/ast.cpp
@@ -0,0 +1,12 @@
+/*
+ * @Author: SiO-2
+ * @Date: 2022-05-09 10:31:29
+ * @LastEditors: SiO-2
+ * @LastEditTime: 2022-05-09 12:51:28
+ * @FilePath: /C-Minus-Compiler/src/ast.cpp
+ * @Description:
+ *
+ * Copyright (c) 2022 by SiO-2, All Rights Reserved.
+ */
+
+#include "ast.hpp"
