c6ecb7f0c5fdd3bed6159e7ecf43c4f673e318de
sio-2 <2586457570@qq.com>
2022-05-12 12:40:57 +0800

    change to n-ary tree.

--- src/ast.hpp
+++ src/ast.hpp
@@ -2,7 +2,7 @@
  * @Author: SiO-2
  * @Date: 2022-05-09 10:31:35
  * @LastEditors: SiO-2
- * @LastEditTime: 2022-05-11 11:44:32
+ * @LastEditTime: 2022-05-12 12:37:20
  * @FilePath: /C-Minus-Compiler/src/ast.hpp
  * @Description: Convert the parse tree to AST for subsequent LLVM operations.
  *
@@ -47,8 +47,8 @@ union ASTNUM
 };
 
 /**
- * @brief ASTNODE, PROGRAM, VARDECL, FUNDECL, PARAM, COMPOUNDSTMT, LOCALDECL, EXPR, VAR,
- *  SIMPLEEXPR, ADDEXPR, TERM, FACTOR, CALL, SELECTSTMT, WHILESTMT, FORSTMT, RETURNSTMT.
+ * @brief ASTNODE, PROGRAM, VARDECL, FUNDECL, PARAM, COMPOUNDSTMT, EXPR, VAR, SIMPLEEXPR,
+ *  ADDEXPR, TERM, FACTOR, CALL, SELECTSTMT, WHILESTMT, FORSTMT, RETURNSTMT.
  */
 enum ASTNodeType
 {
@@ -58,7 +58,6 @@ enum ASTNodeType
     FUNDECL,
     PARAM,
     COMPOUNDSTMT,
-    LOCALDECL,
     EXPR,
     VAR,
     SIMPLEEXPR,
@@ -77,24 +76,20 @@ enum ASTNodeType
  * @param {int} lineno - initialized to -1.
  * @param {int} colume - initialized to -1.
  * @param {ASTNodeType} astNodeType - initialized to ASTNODE.
- * @param {ASTNode*} leftChild - initialized to NULL.
- * @param {ASTNode*} rightSibling - initialized to NULL.
  */
 class ASTNode
 {
     int lineno, column;
     ASTNodeType nodeType;
-    ASTNode *leftChild;
-    ASTNode *rightSibling;
+    // ASTNode *leftChild;
+    // ASTNode *rightSibling;
 
 public:
-    ASTNode()
+    ASTNode(int lineno = -1, int column = -1) : lineno(lineno), column(column)
     {
-        lineno = -1;
-        column = -1;
         nodeType = ASTNODE;
-        leftChild = NULL;
-        rightSibling = NULL;
+        // leftChild = NULL;
+        // rightSibling = NULL;
     }
     virtual ~ASTNode() {}
 
@@ -104,39 +99,72 @@ public:
     void SetColumn(int setColumn) { column = setColumn; }
     int GetColumn() { return column; }
 
+    /**
+     * @brief ASTNodeType - ASTNODE, PROGRAM, VARDECL, FUNDECL, PARAM, COMPOUNDSTMT, LOCALDECL, EXPR, VAR,
+     *  SIMPLEEXPR, ADDEXPR, TERM, FACTOR, CALL, SELECTSTMT, WHILESTMT, FORSTMT, RETURNSTMT.
+     */
     void SetNodeType(ASTNodeType setNodeType) { nodeType = setNodeType; }
     ASTNodeType GetNodeType() { return nodeType; }
 
-    void SetLeftChild(ASTNode *setLeftChild) { leftChild = setLeftChild; }
-    ASTNode *GetLeftChild() { return leftChild; }
+    void SetASTNodeData(int lineno, int column, ASTNodeType nodeType)
+    {
+        ASTNode::lineno = lineno;
+        ASTNode::column = column;
+        ASTNode::nodeType = nodeType;
+    }
 
-    void SetRightSibling(ASTNode *setRightSibling) { rightSibling = setRightSibling; }
-    ASTNode *GetRightSibling() { return rightSibling; }
+    // void SetLeftChild(ASTNode *setLeftChild) { leftChild = setLeftChild; }
+    // ASTNode *GetLeftChild() { return leftChild; }
+
+    // void SetRightSibling(ASTNode *setRightSibling) { rightSibling = setRightSibling; }
+    // ASTNode *GetRightSibling() { return rightSibling; }
 };
 
 /**
  * @brief A program consists of a list (or sequence) of declarations,
  *  which may be function or variable declarations, in any order.
+ * @param {vector<ASTNode*>} Decl - There must be at least one declaration.
+ *  The last declaration in a program must be a function declaration of the form void main(void).
  */
-class ProgramNode : public ASTNode
+class Program : public ASTNode
 {
+    vector<ASTNode *> Decl;
+
 public:
-    ProgramNode() {}
+    Program() {}
 };
 
+/**
+ * @brief A variable declaration declares either a simple variable of integer type or an array
+ *  variable whose base type is integer or float, and whose indices range from 0 ... num-1.
+ * @param {TypeSpec} typeSpec - int, real or void.
+ * @param {string} id - identifier.
+ * @param {bool} isArray - by default is not an array.
+ * @param {int} arrayLength - array length.
+ */
 class VarDecl : public ASTNode
 {
     TypeSpec typeSpec;
     string id;
-    ASTNUM num;
+    bool isArray;
+    int arrayLength;
 
 public:
-    VarDecl(TypeSpec typeSpec, string id, int intNum) : typeSpec(typeSpec), id(id) { num.intNum = intNum; }
-    VarDecl(TypeSpec typeSpec, string id, float floatNum) : typeSpec(typeSpec), id(id) { num.floatNum = floatNum; }
+    VarDecl(TypeSpec typeSpec, string id)
+        : typeSpec(typeSpec), id(id), isArray(false) {}
+
+    VarDecl(TypeSpec typeSpec, string id, int arrayLength)
+        : typeSpec(typeSpec), id(id), isArray(true), arrayLength(arrayLength) {}
 };
 
 /**
- * @brief By default there are no parameters.
+ * @brief Function declaration.
+ * @param {TypeSpec} typeSpec - return type specifier.
+ * @param {string} id - identifier.
+ * @param {bool} haveParam - flag of parameters. By default there are no parameters.
+ * @param {vector<Param*>} paramList - comma-separated list of parameters inside parentheses.
+ * @param {vector<ASTNode*>} compoundStmt - compound statement consists of curly brackets
+ *  surrounding a set of declarations and statements.
  */
 class FunDecl : public ASTNode
 {
@@ -144,18 +172,31 @@ class FunDecl : public ASTNode
     string id;
     bool haveParam; // Is there an param.
     vector<Param *> paramList;
+    vector<ASTNode *> compoundStmt;
 
 public:
-    FunDecl(TypeSpec typeSpec, string id, bool haveParam = false) : typeSpec(typeSpec), id(id), haveParam(haveParam) {}
+    FunDecl(TypeSpec typeSpec, string id, bool haveParam = false)
+        : typeSpec(typeSpec), id(id), haveParam(haveParam) {}
+
     void AddParam(Param *param)
     {
-        haveParam = true;
+        if (!haveParam)
+            haveParam = true;
         paramList.push_back(param);
     }
+
+    void AddStmt(ASTNode *stmt) { compoundStmt.push_back(stmt); };
 };
 
 /**
- * @brief By default is not an array.
+ * @brief Parameters of a function are either void (i.e., there  *  are no parameters) or
+ *  a list representing the function's parameters. Parameters followed by brackets are array
+ *  parameters whose size can vary. Simple integer parameters are passed by value. Array
+ *  parameters are passed by reference (i.e., as pointers) and must be matched by an array
+ *  variable during a call.
+ * @param {TypeSpec} typeSpec - int, real or void.
+ * @param {string} id - identifier.
+ * @param {bool} isArray - by default is not an array.
  */
 class Param : public ASTNode
 {
@@ -164,37 +205,39 @@ class Param : public ASTNode
     bool isArray;
 
 public:
-    Param(TypeSpec typeSpec, string id, bool isArray = false) : typeSpec(typeSpec), id(id), isArray(isArray) {}
+    Param(TypeSpec typeSpec, string id, bool isArray = false)
+        : typeSpec(typeSpec), id(id), isArray(isArray) {}
 };
 
-class CompoundStmt : public ASTNode
-{
-    vector<LocalDecl *> localDeclList;
-    vector<Expr *> exprList;
-    vector<SelectStmt *> selectStmtList;
-    vector<WhileStmt *> whileStmtList;
-    vector<ForStmt *> forStmtList;
-    ReturnStmt *returnStmt;
-
-public:
-    CompoundStmt() {}
-};
-
-class LocalDecl : public ASTNode
-{
-    vector<int> test;
-
-public:
-    LocalDecl() {}
-};
+// class CompoundStmt : public ASTNode
+// {
+//     vector<LocalDecl *> localDeclList;
+//     vector<Expr *> exprList;
+//     vector<SelectStmt *> selectStmtList;
+//     vector<WhileStmt *> whileStmtList;
+//     vector<ForStmt *> forStmtList;
+//     ReturnStmt *returnStmt;
+//     vector<ASTNode *> localDeclAndStmtList;
+// public:
+//     CompoundStmt()
+//     {
+//     }
+// };
 
 class Expr : public ASTNode
 {
-    vector<int> test;
-    bool isAssignStmt; // Is it an assignment statement.
+    bool isAssignStmt; // Is it an assignment statement. By default it is not.
+    Var *var;
+    Expr *expr;
+
+    SimpleExpr *simpleExpr;
 
 public:
-    Expr() {}
+    Expr(SimpleExpr *simpleExpr)
+        : isAssignStmt(false), var(NULL), expr(NULL), simpleExpr(simpleExpr) {}
+
+    Expr(Var *var, Expr *expr)
+        : isAssignStmt(true), var(var), expr(expr), simpleExpr(NULL) {}
 };
 
 class Var : public ASTNode
