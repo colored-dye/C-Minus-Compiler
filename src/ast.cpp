22c0f68895f48b9a77fb549a50d483f56a145e96
sio-2 <2586457570@qq.com>
2022-05-14 00:33:42 +0800

    init

--- src/ast.cpp
+++ src/ast.cpp
@@ -2,7 +2,7 @@
  * @Author: SiO-2
  * @Date: 2022-05-09 10:31:29
  * @LastEditors: SiO-2
- * @LastEditTime: 2022-05-12 23:35:33
+ * @LastEditTime: 2022-05-14 00:33:02
  * @FilePath: /C-Minus-Compiler/src/ast.cpp
  * @Description:
  *
@@ -11,4 +11,5 @@
 
 #include "ast.hpp"
 
-extern const char *Nodename[];
+#include <iostream>
+using namespace std;
