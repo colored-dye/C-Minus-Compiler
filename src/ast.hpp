9842b5de1414b65a69c0bf696564e638701da13b
sio-2 <2586457570@qq.com>
2022-05-14 18:35:36 +0800

    modified destructor

--- src/ast.hpp
+++ src/ast.hpp
@@ -2,7 +2,7 @@
  * @Author: SiO-2
  * @Date: 2022-05-09 10:31:35
  * @LastEditors: SiO-2
- * @LastEditTime: 2022-05-14 00:31:09
+ * @LastEditTime: 2022-05-14 18:33:09
  * @FilePath: /C-Minus-Compiler/src/ast.hpp
  * @Description: AST for subsequent LLVM operations.
  *
@@ -12,7 +12,7 @@
 #ifndef CMinusCompiler_AST_H_
 #define CMinusCompiler_AST_H_
 
-#include "node.h"
+#include "node.hpp"
 
 #include <vector>
 #include <string>
@@ -171,18 +171,7 @@ class Program : public ASTNode
 
 public:
     Program() {}
-    ~Program()
-    {
-        vector<ASTNode *>::iterator decl;
-        for (decl = declList.begin(); decl != declList.end(); decl++)
-        {
-            if ((*decl)->GetNodeType() == ASTVARDECL)
-                ((VarDecl *)(*decl))->~VarDecl();
-            else
-                ((FunDecl *)(*decl))->~FunDecl();
-            declList.clear();
-        }
-    }
+    ~Program() {}
 
     void AddDecl(ASTNode *decl) { declList.push_back(decl); }
     const vector<ASTNode *> &GetDeclList() const { return declList; }
@@ -238,44 +227,7 @@ class FunDecl : public ASTNode
 public:
     FunDecl(ASTTypeSpec typeSpec, string id, bool haveParam = false)
         : typeSpec(typeSpec), id(id), haveParam(haveParam) {}
-
-    ~FunDecl()
-    {
-        if (haveParam)
-        {
-            vector<Param *>::iterator param;
-            for (param = paramList.begin(); param != paramList.end(); param++)
-                (*param)->~Param();
-            paramList.clear();
-        }
-
-        vector<ASTNode *>::iterator stmt;
-        for (stmt = compoundStmt.begin(); stmt != compoundStmt.end(); stmt++)
-        {
-            switch ((*stmt)->GetNodeType())
-            {
-            case ASTVARDECL:
-                ((VarDecl *)(*stmt))->~VarDecl();
-                break;
-            case ASTEXPR:
-                ((Expr *)(*stmt))->~Expr();
-                break;
-            case ASTSELECTSTMT:
-                ((SelectStmt *)(*stmt))->~SelectStmt();
-                break;
-            case ASTWHILESTMT:
-                ((WhileStmt *)(*stmt))->~WhileStmt();
-                break;
-            case ASTRETURNSTMT:
-                ((ReturnStmt *)(*stmt))->~ReturnStmt();
-                break;
-
-            default:
-                break;
-            }
-        }
-        compoundStmt.clear();
-    }
+    ~FunDecl() {}
 
     void AddParam(Param *param)
     {
@@ -342,16 +294,7 @@ public:
     Expr(Var *var, Expr *expr)
         : isAssignStmt(true), var(var), expr(expr), simpleExpr(NULL) {}
 
-    ~Expr()
-    {
-        if (isAssignStmt)
-        {
-            var->~Var();
-            expr->~Expr();
-        }
-        else
-            simpleExpr->~SimpleExpr();
-    }
+    ~Expr() {}
 
     bool IsAssignStmt() const { return isAssignStmt; }
     const Var *GetVar() const { return var; }
@@ -377,71 +320,9 @@ class SelectStmt : public ASTNode
 
 public:
     SelectStmt(Expr *expr = NULL) : expr(expr) { haveElse = false; }
-    ~SelectStmt()
-    {
-        expr->~Expr();
-        vector<ASTNode *>::iterator trueStmt;
-        for (trueStmt = trueCompoundStmt.begin(); trueStmt != trueCompoundStmt.end(); trueStmt++)
-        {
-            switch ((*trueStmt)->GetNodeType())
-            {
-            case ASTVARDECL:
-                ((VarDecl *)(*trueStmt))->~VarDecl();
-                break;
-            case ASTEXPR:
-                ((Expr *)(*trueStmt))->~Expr();
-                break;
-            case ASTSELECTSTMT:
-                ((SelectStmt *)(*trueStmt))->~SelectStmt();
-                break;
-            case ASTWHILESTMT:
-                ((WhileStmt *)(*trueStmt))->~WhileStmt();
-                break;
-            case ASTRETURNSTMT:
-                ((ReturnStmt *)(*trueStmt))->~ReturnStmt();
-                break;
+    ~SelectStmt() {}
 
-            default:
-                break;
-            }
-        }
-        trueCompoundStmt.clear();
-
-        if (haveElse)
-        {
-            vector<ASTNode *>::iterator falseStmt;
-            for (falseStmt = falseCompoundStmt.begin(); falseStmt != falseCompoundStmt.end(); falseStmt++)
-            {
-                switch ((*falseStmt)->GetNodeType())
-                {
-                case ASTVARDECL:
-                    ((VarDecl *)(*falseStmt))->~VarDecl();
-                    break;
-                case ASTEXPR:
-                    ((Expr *)(*falseStmt))->~Expr();
-                    break;
-                case ASTSELECTSTMT:
-                    ((SelectStmt *)(*falseStmt))->~SelectStmt();
-                    break;
-                case ASTWHILESTMT:
-                    ((WhileStmt *)(*falseStmt))->~WhileStmt();
-                    break;
-                case ASTRETURNSTMT:
-                    ((ReturnStmt *)(*falseStmt))->~ReturnStmt();
-                    break;
-
-                default:
-                    break;
-                }
-            }
-            falseCompoundStmt.clear();
-        }
-    }
-
-    void AddTrueStmt(ASTNode *stmt)
-    {
-        trueCompoundStmt.push_back(stmt);
-    }
+    void AddTrueStmt(ASTNode *stmt) { trueCompoundStmt.push_back(stmt); }
     void AddFalseStmt(ASTNode *stmt)
     {
         haveElse = true;
@@ -468,37 +349,7 @@ class WhileStmt : public ASTNode
 
 public:
     WhileStmt(Expr *expr = NULL) : expr(expr) {}
-    ~WhileStmt()
-    {
-        expr->~Expr();
-
-        vector<ASTNode *>::iterator stmt;
-        for (stmt = compoundStmt.begin(); stmt != compoundStmt.end(); stmt++)
-        {
-            switch ((*stmt)->GetNodeType())
-            {
-            case ASTVARDECL:
-                ((VarDecl *)(*stmt))->~VarDecl();
-                break;
-            case ASTEXPR:
-                ((Expr *)(*stmt))->~Expr();
-                break;
-            case ASTSELECTSTMT:
-                ((SelectStmt *)(*stmt))->~SelectStmt();
-                break;
-            case ASTWHILESTMT:
-                ((WhileStmt *)(*stmt))->~WhileStmt();
-                break;
-            case ASTRETURNSTMT:
-                ((ReturnStmt *)(*stmt))->~ReturnStmt();
-                break;
-
-            default:
-                break;
-            }
-        }
-        compoundStmt.clear();
-    }
+    ~WhileStmt() {}
 
     void AddStmt(ASTNode *stmt) { compoundStmt.push_back(stmt); }
 
@@ -533,49 +384,7 @@ class ForStmt : public ASTNode
 public:
     ForStmt() : haveForParam1(false), var1(NULL), expr1(NULL), expr2(NULL),
                 haveForParam3(false), var3(NULL), expr3(NULL) {}
-    ~ForStmt()
-    {
-        if (haveForParam1)
-        {
-            var1->~Var();
-            expr1->~Expr();
-        }
-
-        expr2->~Expr();
-
-        if (haveForParam3)
-        {
-            var3->~Var();
-            expr3->~Expr();
-        }
-
-        vector<ASTNode *>::iterator stmt;
-        for (stmt = compoundStmt.begin(); stmt != compoundStmt.end(); stmt++)
-        {
-            switch ((*stmt)->GetNodeType())
-            {
-            case ASTVARDECL:
-                ((VarDecl *)(*stmt))->~VarDecl();
-                break;
-            case ASTEXPR:
-                ((Expr *)(*stmt))->~Expr();
-                break;
-            case ASTSELECTSTMT:
-                ((SelectStmt *)(*stmt))->~SelectStmt();
-                break;
-            case ASTWHILESTMT:
-                ((WhileStmt *)(*stmt))->~WhileStmt();
-                break;
-            case ASTRETURNSTMT:
-                ((ReturnStmt *)(*stmt))->~ReturnStmt();
-                break;
-
-            default:
-                break;
-            }
-        }
-        compoundStmt.clear();
-    }
+    ~ForStmt() {}
 
     void AddForParam1(Var *var1, Expr *expr1)
     {
@@ -624,11 +433,7 @@ class ReturnStmt : public ASTNode
 
 public:
     ReturnStmt(bool isVoid = true) : isVoid(isVoid), expr(NULL) {}
-    ~ReturnStmt()
-    {
-        if (!isVoid)
-            expr->~Expr();
-    }
+    ~ReturnStmt() {}
 
     void AddExpr(Expr *expr)
     {
@@ -658,11 +463,7 @@ public:
     Var(string id) : id(id), haveSubscript(false), subscript(NULL) {}
     Var(string id, Expr *subscript) : id(id), haveSubscript(true), subscript(subscript) {}
 
-    ~Var()
-    {
-        if (haveSubscript)
-            subscript->~Expr();
-    }
+    ~Var() {}
 
     const string GetId() const { return id; }
     bool HaveSubscript() const { return haveSubscript; }
@@ -689,12 +490,7 @@ public:
     SimpleExpr(AddExpr *addExpr) : leftAddExpr(addExpr), haveRightAddExpr(false) { rightAddExpr = NULL; }
     SimpleExpr(AddExpr *leftAddExpr, ASTRelOp relOp, AddExpr *rightAddExpr)
         : leftAddExpr(leftAddExpr), haveRightAddExpr(true), relOp(relOp), rightAddExpr(rightAddExpr) {}
-    ~SimpleExpr()
-    {
-        leftAddExpr->~AddExpr();
-        if (haveRightAddExpr)
-            rightAddExpr->~AddExpr();
-    }
+    ~SimpleExpr() {}
 
     const AddExpr *GetLeftAddExpr() const { return leftAddExpr; }
     bool HaveRightAddExpr() const { return haveRightAddExpr; }
@@ -718,18 +514,7 @@ class AddExpr : public ASTNode
 
 public:
     AddExpr(Term *firstTerm) : firstTerm(firstTerm), areMultipleTerms(false) {}
-    ~AddExpr()
-    {
-        firstTerm->~Term();
-        if (areMultipleTerms)
-        {
-            addOpList.clear();
-            vector<Term *>::iterator term;
-            for (term = termList.begin(); term != termList.end(); term++)
-                (*term)->~Term();
-            termList.clear();
-        }
-    }
+    ~AddExpr() {}
 
     void AddTerm(ASTAddOp addOp, Term *term)
     {
@@ -762,18 +547,7 @@ class Term : public ASTNode
 
 public:
     Term(Factor *firstFactor) : firstFactor(firstFactor), areMultipleFactors(false) {}
-    ~Term()
-    {
-        firstFactor->~Factor();
-        if (areMultipleFactors)
-        {
-            mulOpList.clear();
-            vector<Factor *>::iterator factor;
-            for (factor = factorList.begin(); factor != factorList.end(); factor++)
-                (*factor)->~Factor();
-            factorList.clear();
-        }
-    }
+    ~Term() {}
 
     void AddTerm(ASTMulOp mulOp, Factor *factor)
     {
@@ -813,15 +587,7 @@ public:
     Factor(Call *callExpr) : expr(NULL), var(NULL), callExpr(callExpr), isNum(false) {}
     Factor(ASTNUM num) : expr(NULL), var(NULL), callExpr(NULL), isNum(true), num(num) {}
 
-    ~Factor()
-    {
-        if (expr != NULL)
-            expr->~Expr();
-        if (var != NULL)
-            var->~Var();
-        if (callExpr != NULL)
-            callExpr->~Call();
-    }
+    ~Factor() {}
 
     const Expr *GetExpr() const { return expr; }
     const Var *GetVar() const { return var; }
@@ -842,13 +608,7 @@ class Call : public ASTNode
 
 public:
     Call(string id) : id(id) {}
-    ~Call()
-    {
-        vector<Expr *>::iterator arg;
-        for (arg = argList.begin(); arg != argList.end(); arg++)
-            (*arg)->~Expr();
-        argList.clear();
-    }
+    ~Call() {}
 
     void AddArg(Expr *arg) { argList.push_back(arg); }
 
